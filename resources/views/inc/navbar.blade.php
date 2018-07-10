65d5fd09cb43d2fe3edbe79e0f9764a7e5b4733d
Allan Ter <terguoheng@hotmail.com>
2018-07-11 01:15:52 +0800

    Redecorated navbar.blade file
    Also added navbar-right, a create post link.

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,24 +1,26 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">{{config('app.name', 'LSAPP')}}</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+
+<a class="navbar-brand" href="/">{{config('app.name', 'LSAPP')}}</a>
+
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span></button>
+
       
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li>
-              <a class="nav-link" href="/">Home</a></li>
-            
-            <li>
-              <a class="nav-link" href="/about">About</a>
-            </li>
-            <li>
-            <a class="nav-link" href="/services">Services</a>
-                </li>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-            <li>
-            <a class="nav-link" href="/posts">Blog</a>
-                </li>
+        <ul class="navbar-nav mr-auto">
+        
+        <li><a class="nav-link" href="/">Home</a></li>  
+        <li><a class="nav-link" href="/about">About</a></li>
+        <li><a class="nav-link" href="/services">Services</a></li>
+        <li><a class="nav-link" href="/posts">Blog</a></li>
 
-        </div>
-      </nav>
+        </ul>
+
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="/posts/create">Create Post</a></li>
+        </ul>
+
+
+    </div>
+</nav>
